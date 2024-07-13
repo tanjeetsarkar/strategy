ff55ef2df2b822a2afb792787f9170b20a27d69f
Tanjeet Sarkar <tanjeetsarakarofficial@gmail.com>
2024-07-13 19:44:38 +0530

    change graph logic

--- draw.py
+++
@@ -1,7 +0,0 @@
-def draw_graph():
-    canvas = [['*' for _ in range(10)] for _ in range(10)]
-    for row in canvas:
-        print(''.join(row))
-
-
-draw_graph()
--- main.py
+++ main.py
@@ -2,70 +2,45 @@
 # each node will contain links to other nodes in list (as it can be changed). 
 # each node will have a call back function which will define the functionality of the node.
 
-
-from typing import List
-import random
 import uuid
+import random
 
 class Node:
 
-    def __init__(self, data, inward:list = [], outward:list=[]) -> None:
-        self.data = data
-        self.inward = inward
-        self.outward = outward
-
-    def node_type(self):
-        if len(self.outward) and not len(self.inward):
-            return "INPUT"
-        elif len(self.inward) and not len(self.outward):
-            return "OUTPUT"
-        elif not len(self.inward) and not len(self.outward):
-            return "ISOLATED"
-        else:
-            return "PROCESSING"
-
-    def clear_links(self):
-        self.inward = []
-        self.outward = []
+    def __init__(self, decision) -> None:
+        self.decision = decision
+        self.id = uuid.uuid4().hex
 
     def __repr__(self) -> str:
-        return uuid.uuid4().hex
+        return self.decision
 
-class StrategyGraph:
+class Link:
 
-    def __init__(self, nodes: List[Node]= []) -> None:
-        self.nodes = nodes
+    def __init__(self, left:Node, right:Node) -> None:
+        self.left = left
+        self.right = right
 
-    def add_node(self, data, from_:List[Node] = [], to_:List[Node] = []) -> Node:
-        new_node = Node(data=data, inward=from_, outward=to_)
-        self.nodes.append(new_node)
-        return new_node
+class Graph:
+    def __init__(self, node_count:int, edge_count:int) -> None:
+        self.node_count = node_count
+        self.edge_count = edge_count
 
-    def del_node(self, node:Node):
-        froms = node.inward
-        tos = node.outward
-        for x_node in froms:
-            print("Node Type:", x_node.node.node_type)
-            x_node.outward.extend(tos)
-        for y_node in tos:
-            print("Node Type:", y_node.node.node_type)
-            y_node.inward.extend(froms)
-        node.clear_links()
 
-    def print_graph(self):
-        for node in self.nodes:
-            print("\t",node,node.node_type(), " <------ ", node.inward)
-            print("\t","|")
-            print("\t","------> ", node.outward)
-            print()
+    def create(self):
+        graph: dict[Node, list[Node]] = {}
+
+        for i in range(self.node_count):
+            node = Node(decision=f"Node {i}")
+            graph[node] = []
+
+        for _ in range(self.edge_count):
+            x,y = random.sample(list(graph.keys()), 2)
+            graph[x].append(y)
+
+        return graph
+
 
 if __name__ == "__main__":
-    sg = StrategyGraph()
-    for i in range(10):
-        from_nodes = random.sample(sg.nodes, random.randint(0,i))
-        to_nodes = random.sample(sg.nodes, random.randint(0,i))
-        sg.add_node(i, from_=from_nodes, to_=to_nodes)
-    sg.print_graph()
-
-
-
+    g = Graph(8,20)
+    graph = g.create()
+    print(graph)
